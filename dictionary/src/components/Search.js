90fb1f2f030c650d91080eaefe80d86cafe45cd6
havoriskova <ha.voriskova@gmail.com>
2022-12-19 09:34:01 +0100

    Update Search.js

--- dictionary/src/components/Search.js
+++ dictionary/src/components/Search.js
@@ -2,24 +2,23 @@ import {useState} from 'react';
 
 const Search = () => {
 
-    const [input, setInput] = useState('');
-    const [word, setWord] = useState('');
+    const [keyword, setKeyword] = useState('');
 
-    const updateInput = (e) => {
-        setInput(e.target.value);
+    const updateKeyword = (e) => {
+        setKeyword(e.target.value);
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setWord(input);
-        console.log(word);
+        console.log(keyword);
+
     }
 
     return(
         <div className="Search container">
             <form onSubmit={handleSubmit}>
                 <input type='search' placeholder="Type a word" autoFocus
-                onChange={updateInput}></input>
+                onChange={updateKeyword}></input>
             </form>
         </div>
     )
