4aa0c81621e28164d07875dd17d8d10e6d04ff6d
havoriskova <ha.voriskova@gmail.com>
2023-01-04 23:36:22 +0100

    small update
    - next: e.target.dataset.src -> new element

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,8 +4,13 @@ const Gallery = ({pictures}) => {
   
 // pictures je array, kde potrebuju dat max. pocet, napr. 6 -> slice method
 //                  ALE primo v API call muzu dat per_page=6
-// onClick function -> vytvoreni noveho elementu pres celou stranku na picture.src.original
+// onClick function -> vytvoreni noveho elementu pres celou stranku na picture.src.landscape
 //                     + sipky doleva doprava
+
+    const handleClick = (e) => {
+      console.log(e.target.dataset.src);
+
+    }
 
 
     if (pictures) {
@@ -13,7 +18,7 @@ const Gallery = ({pictures}) => {
             <div className="Gallery container content-container">
                 { pictures.map((picture, index) => { 
                     return (<div  className="picture-container" key={index}>
-                        <img alt="" src={picture.src.medium} />
+                        <img alt="" src={picture.src.large} data-src={picture.src.original} onClick={handleClick} />
                     </div>)})
                 }
             </div>
