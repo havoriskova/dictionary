167d9ecbf74957ff68acce8d04cfcd0c4b153158
havoriskova <ha.voriskova@gmail.com>
2023-05-26 11:26:38 +0200

    Update fetchAPI.js

--- netlify/functions/fetchAPI/fetchAPI.js
+++ netlify/functions/fetchAPI/fetchAPI.js
@@ -3,11 +3,25 @@
 const handler = async (event) => {
   try {
     const keyword = event.body || 'yoga';
-    
+    const url = `https://api.pexels.com/v1/search?query=${keyword}&orientation=landscape&per_page=6`;
+
+
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+          "Authorization": `${process.env.REACT_APP_PEXEL_API_KEY}`
+      },
+  
+  });
+
+  
     return {
       statusCode: 200,
-      path: `https://api.pexels.com/v1/search?query=${keyword}&orientation=landscape&per_page=6`,
-      headers: {"Authorization": `${process.env.REACT_APP_PEXEL_API_KEY}`},
+      body: JSON.stringify({
+        reply: response.data
+      })
+      
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
