2c55731dc86fe5d6b5a5084d44d623daf52449ac
havoriskova <ha.voriskova@gmail.com>
2023-06-12 22:55:32 +0200

    Update fetchAPI.js

--- netlify/functions/fetchAPI/fetchAPI.js
+++ netlify/functions/fetchAPI/fetchAPI.js
@@ -2,24 +2,23 @@
 import axios from 'axios';
 
 const handler = async (event) => {
-  try {
+  
     const keyword = event.body || 'yoga';
     const url = `https://api.pexels.com/v1/search?query=${keyword}&orientation=landscape&per_page=6`;
 
 
-    let reply = axios.get(url, {
+   axios.get(url, {
       headers: {
           'Content-Type': 'application/json',
           "Authorization": `${process.env.REACT_APP_PEXEL_API_KEY}`
       },
   
-    });
+    }).then((data) => data)
+      .catch((error) => {console.log(error)});
 
-    return reply;
+
       
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
+ 
 }
 
 module.exports = { handler }
