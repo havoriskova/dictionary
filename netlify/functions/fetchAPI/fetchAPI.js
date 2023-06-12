8308c44afb35965c60d611bf2214c5affb61897f
havoriskova <ha.voriskova@gmail.com>
2023-06-12 23:03:29 +0200

    Update fetchAPI.js

--- netlify/functions/fetchAPI/fetchAPI.js
+++ netlify/functions/fetchAPI/fetchAPI.js
@@ -1,34 +1,41 @@
 // // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+// export const handler = async () => {
+// 	return {
+// 		statusCode: 200,
+// 		body: JSON.stringify({
+// 			message: 'This is what will be returned!'
+// 		})
+// 	}
+// }
+
+//---------------------------------------------------------------------
+
 // import axios from 'axios';
 
 // const handler = async (event) => {
-  
 //     const keyword = event.body || 'yoga';
 //     const url = `https://api.pexels.com/v1/search?query=${keyword}&orientation=landscape&per_page=6`;
-
-
 //    axios.get(url, {
 //       headers: {
 //           'Content-Type': 'application/json',
 //           "Authorization": `${process.env.REACT_APP_PEXEL_API_KEY}`
 //       },
-  
 //     }).then((data) => data)
 //       .catch((error) => {console.log(error)});
-
-
-      
- 
 // }
-
 // module.exports = { handler }
 
 
-export const handler = async () => {
+export const handler = async (event) => {
+  const keyword = event.body || 'yoga';
+  const url = `https://api.pexels.com/v1/search?query=${keyword}&orientation=landscape&per_page=6`;
+
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
-			message: 'This is what will be returned!'
+			message: 'This is what will be returned!',
+      zkouska: keyword,
+      url: url 
 		})
 	}
 }
