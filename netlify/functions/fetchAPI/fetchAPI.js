88143d2e1e1f5ced9fd6443c4e2b1ab89fac04bc
havoriskova <ha.voriskova@gmail.com>
2023-06-12 22:58:43 +0200

    Update fetchAPI.js

--- netlify/functions/fetchAPI/fetchAPI.js
+++ netlify/functions/fetchAPI/fetchAPI.js
@@ -1,28 +1,37 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-import axios from 'axios';
+// // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+// import axios from 'axios';
 
-const handler = async (event) => {
+// const handler = async (event) => {
   
-    const keyword = event.body || 'yoga';
-    const url = `https://api.pexels.com/v1/search?query=${keyword}&orientation=landscape&per_page=6`;
+//     const keyword = event.body || 'yoga';
+//     const url = `https://api.pexels.com/v1/search?query=${keyword}&orientation=landscape&per_page=6`;
 
 
-   axios.get(url, {
-      headers: {
-          'Content-Type': 'application/json',
-          "Authorization": `${process.env.REACT_APP_PEXEL_API_KEY}`
-      },
+//    axios.get(url, {
+//       headers: {
+//           'Content-Type': 'application/json',
+//           "Authorization": `${process.env.REACT_APP_PEXEL_API_KEY}`
+//       },
   
-    }).then((data) => data)
-      .catch((error) => {console.log(error)});
+//     }).then((data) => data)
+//       .catch((error) => {console.log(error)});
 
 
       
  
+// }
+
+// module.exports = { handler }
+
+
+export const handler = async () => {
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: 'This is what will be returned!'
+		})
+	}
 }
-
-module.exports = { handler }
-
 
   // let apiUrlPexel = `https://api.pexels.com/v1/search?query=${keyword}&orientation=landscape&per_page=6`;
         // axios.get(apiUrlPexel, {headers: {"Authorization": `${apiPexelKey}`}})
